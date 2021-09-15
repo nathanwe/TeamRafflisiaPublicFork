2a4d1ed258c56f44b3e6f33df365f43a91c5924c
gerald-lee <cwlee2000123@gmail.com>
2021-09-14 19:09:55 -0700

    added framerate controller (#4)

---
+++ GTXDankEngine/RunningAverage.h
@@ -0,0 +1,42 @@
+#ifndef RUNNING_AVERAGE_H
+#define RUNNING_AVERAGE_H
+
+#include <cstdint>
+#include <queue>
+
+template <typename T, typename TDivisor = std::uint32_t>
+class RunningAverage
+{
+public:
+    RunningAverage(std::uint32_t numberTracked, T initialAverage)
+        :
+        average(initialAverage),
+        lastTotal(average* (TDivisor)numberTracked),
+        numberTracked(numberTracked)
+    {
+        while (numberTracked-- > 0)
+            pastValues.push(average);
+    }
+
+    // Updates the running average, and returns it
+    T Update(T value)
+    {
+        lastTotal -= pastValues.front();
+        lastTotal += value;
+        pastValues.pop();
+        pastValues.push(value);
+        average = lastTotal / (TDivisor)numberTracked;
+        return average;
+    }
+
+    T average() { return average; }
+
+private:
+    T average;
+    T lastTotal;
+    std::queue<T> pastValues;
+    std::uint32_t numberTracked;
+};
+
+
+#endif
---
+++ GTXDankEngine/Source/FramerateController.cpp
@@ -0,0 +1,101 @@
+#include "FramerateController.h"
+
+using float_seconds = std::chrono::duration<float>;
+
+FramerateController::FramerateController(std::uint32_t targetFramerate) :
+	targetFramerate(targetFramerate),
+	frameAverage(10, precision(0)),
+	smoothedDeltaTime(precision(0)),
+	deltaTime(precision(0))
+{
+	auto frameTime = std::chrono::duration<double>(1) / targetFramerate;
+	targetFrameTime = std::chrono::duration_cast<std::chrono::nanoseconds>(frameTime);
+}
+
+void FramerateController::BeginTotal()
+{
+	beginTotal = timer.now();
+}
+
+void FramerateController::StartFrame()
+{
+	start = timer.now();
+}
+
+void FramerateController::EndFrame()
+{
+	Wait();
+
+	end = timer.now();
+	deltaTime = end - start;
+	smoothedDeltaTime = frameAverage.Update(deltaTime);
+
+	LOG_INFO(FrameInfo());
+}
+
+std::chrono::nanoseconds FramerateController::DeltaTime() const
+{
+	return deltaTime;
+}
+
+std::chrono::nanoseconds FramerateController::SmoothedDeltaTime() const
+{
+	return smoothedDeltaTime;
+}
+
+float FramerateController::DeltaSeconds() const
+{
+	return std::chrono::duration_cast<float_seconds>(deltaTime).count();
+}
+
+float FramerateController::SmoothedDeltaSeconds() const
+{
+	return std::chrono::duration_cast<float_seconds>(smoothedDeltaTime).count();
+}
+
+float FramerateController::CurrentFramerate() const
+{
+	return 1 / DeltaSeconds();
+}
+
+float FramerateController::SmoothedCurrentFramerate() const
+{
+	return 1 / SmoothedDeltaSeconds();
+}
+
+std::chrono::nanoseconds FramerateController::CurrentFrameTime() const
+{
+	return std::chrono::high_resolution_clock::now() - start;
+}
+
+float FramerateController::CurrentFrameSeconds() const
+{
+
+	return std::chrono::duration_cast<float_seconds>(CurrentFrameTime()).count();
+}
+
+std::string FramerateController::FrameInfo() const
+{
+	return "delta: " + std::to_string(DeltaTime().count()) + "ns\n" +
+		"smoothed framerate: " + std::to_string(SmoothedCurrentFramerate()) + "\n" +
+		"current framerate: " + std::to_string(CurrentFramerate()) + "\n";
+}
+
+float FramerateController::TotalSeconds() const
+{
+	auto diff = start - beginTotal;
+	return std::chrono::duration_cast<float_seconds>(diff).count();
+}
+
+void FramerateController::Wait()
+{
+	auto waitTime = targetFrameTime - CurrentFrameTime();
+
+	auto startWait = std::chrono::high_resolution_clock::now();
+	while (true)
+	{
+		auto now = std::chrono::high_resolution_clock::now();
+		if (now - startWait > waitTime) 
+			break;
+	}
+}
