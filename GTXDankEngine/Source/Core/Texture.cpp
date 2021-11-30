546df0cda25e450bd5466a99189aee224b2e85c7
cjmms <brandon.xyw@gmail.com>
2021-11-30 13:30:59 -0800

    CrossHair (#165)

--- GTXDankEngine/Source/Core/Texture.cpp
+++ GTXDankEngine/Source/Core/Texture.cpp
@@ -22,6 +22,9 @@ void Texture::OnLoad()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+
 	if (numColCh == 4)
 	{
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImg, heightImg, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
