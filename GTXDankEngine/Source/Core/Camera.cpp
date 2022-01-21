5b50fdca7857380875f9edca90788391c5701172
dylanw3000 <dylanw3000@gmail.com>
2022-01-21 10:15:04 -0800

    Advanced Camera controls (#197)

--- GTXDankEngine/Source/Core/Camera.cpp
+++ GTXDankEngine/Source/Core/Camera.cpp
@@ -27,28 +27,32 @@ Camera::Camera(int width, int height, glm::vec3 position)
 void Camera::Init()
 {
 	/*
-	autoScroll = true;
-	autoScrollMaxTime = 10.f;
-	autoScrollTimer = 0.f;
-	autoScrollBegin = glm::vec3(0, 5, -20);
-	autoScrollDest = glm::vec3(-5, -5, 5);
+	std::vector<glm::vec3> p{ {0,0,0}, {5,5,5}, {30, 0, 5} };
+
+	std::vector<float> a{ 0, -1.57 };
+	std::vector<float> b{ 0, 0 };
+
+	createCameraSpline(3.f, p, a,b);
 	*/
 }
 
 void Camera::Update(float dt)
 {
 	autoScrollTimer += dt;
+	railTimer += dt;
 }
 
 void Camera::UpdateMatrix(float FOVdeg, float nearPlane, float farPlane)
 {
+	if (isStatic) { return; }
+
 	viewMatrix = glm::mat4(1.0f);
 	projectionMatrix = glm::mat4(1.0f);
 
 	// view = glm::lookAt(Position, Position + Orientation, Up);
 	if (thirdPerson) {
 		if (isOffset) {
-			viewMatrix = glm::translate(glm::mat4(1.f), glm::vec3(0, 0, -thirdPersonOffset)) * glm::mat4_cast(orientationQuat) * glm::translate(glm::mat4(1.0f), -Position - offset*orientationQuat);
+			viewMatrix = glm::translate(glm::mat4(1.f), glm::vec3(0, 0, -thirdPersonOffset)) * glm::mat4_cast(orientationQuat) * glm::translate(glm::mat4(1.0f), -Position - offset * orientationQuat);
 		}
 		else {
 			viewMatrix = glm::translate(glm::mat4(1.f), glm::vec3(0, 0, -thirdPersonOffset)) * glm::mat4_cast(orientationQuat) * glm::translate(glm::mat4(1.0f), -Position);
@@ -56,7 +60,7 @@ void Camera::UpdateMatrix(float FOVdeg, float nearPlane, float farPlane)
 	}
 	else {
 		if (isOffset) {
-			viewMatrix = glm::mat4_cast(orientationQuat) * glm::translate(glm::mat4(1.0f), -Position - offset*orientationQuat);
+			viewMatrix = glm::mat4_cast(orientationQuat) * glm::translate(glm::mat4(1.0f), -Position - offset * orientationQuat);
 		}
 		else {
 			viewMatrix = glm::mat4_cast(orientationQuat) * glm::translate(glm::mat4(1.0f), -Position);
@@ -76,6 +80,8 @@ void Camera::Inputs(GLFWwindow* window)
 {
 	bool updated = false;
 
+	if (isStatic) { return; }
+
 	//if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS)
 	//{
 		//if (firstClick)
@@ -84,56 +90,56 @@ void Camera::Inputs(GLFWwindow* window)
 		//	firstClick = false;
 		//}
 
-		updated = true;
+	updated = true;
 
-		double mouseX = 0;
-		double mouseY = 0;
+	double mouseX = 0;
+	double mouseY = 0;
 
-		glfwGetCursorPos(window, &mouseX, &mouseY);
+	glfwGetCursorPos(window, &mouseX, &mouseY);
 
-		float rotX = sensitivity * (float)(mouseX - (width / 2)) / width;
-		float rotY = sensitivity * (float)(mouseY - (height / 2)) / height;
-		glfwSetCursorPos(window, (width / 2), (height / 2));
+	float rotX = sensitivity * (float)(mouseX - (width / 2)) / width;
+	float rotY = sensitivity * (float)(mouseY - (height / 2)) / height;
+	glfwSetCursorPos(window, (width / 2), (height / 2));
 
-		if (engine.CommandSys.setting.mouseInvertedX) rotX *= -1.f;
-		if (engine.CommandSys.setting.mouseInvertedY) rotY *= -1.f;
+	if (engine.CommandSys.setting.mouseInvertedX) rotX *= -1.f;
+	if (engine.CommandSys.setting.mouseInvertedY) rotY *= -1.f;
 
-		pitch += rotY;
-		yaw += rotX;
+	pitch += rotY;
+	yaw += rotX;
 
-		// Support for sound system; TODO remove
-		Orientation = glm::rotate(Orientation, glm::radians(-rotX * 180.f), glm::normalize(glm::cross(Orientation, Up)));
-		Orientation = glm::rotate(Orientation, glm::radians(-rotY * 180.f), Up);
+	// Support for sound system; TODO remove
+	Orientation = glm::rotate(Orientation, glm::radians(-rotX * 180.f), glm::normalize(glm::cross(Orientation, Up)));
+	Orientation = glm::rotate(Orientation, glm::radians(-rotY * 180.f), Up);
 	//}
 	//else if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_RELEASE) {
 		//firstClick = true;
 
-		if (engine.InputSys.IsControllerActive(0)) {
-			updated = true;
+	if (engine.InputSys.IsControllerActive(0)) {
+		updated = true;
 
-			float rotX = 0.f;
-			float rotY = 0.f;
+		float rotX = 0.f;
+		float rotY = 0.f;
 
-			if (abs(engine.InputSys.GetControllerAxis(0, 2)) >= 0.1f) {
-				rotX = gamepadSensitivity * engine.InputSys.GetControllerAxis(0, 2);
-				if (engine.CommandSys.setting.gamepadInvertedX) {
-					rotX *= -1.f;
-				}
+		if (abs(engine.InputSys.GetControllerAxis(0, 2)) >= 0.1f) {
+			rotX = gamepadSensitivity * engine.InputSys.GetControllerAxis(0, 2);
+			if (engine.CommandSys.setting.gamepadInvertedX) {
+				rotX *= -1.f;
 			}
-			if (abs(engine.InputSys.GetControllerAxis(0, 3)) >= 0.1f) {
-				rotY = gamepadSensitivity * engine.InputSys.GetControllerAxis(0, 3);
-				if (engine.CommandSys.setting.gamepadInvertedY) {
-					rotY *= -1.f;
-				}
-			}
-
-			// Support for sound system; TODO remove
-			Orientation = glm::rotate(Orientation, glm::radians(180 * -rotX), glm::normalize(glm::cross(Orientation, Up)));
-			Orientation = glm::rotate(Orientation, glm::radians(180 * -rotY), Up);
-
-			yaw += rotX;
-			pitch += rotY;
 		}
+		if (abs(engine.InputSys.GetControllerAxis(0, 3)) >= 0.1f) {
+			rotY = gamepadSensitivity * engine.InputSys.GetControllerAxis(0, 3);
+			if (engine.CommandSys.setting.gamepadInvertedY) {
+				rotY *= -1.f;
+			}
+		}
+
+		// Support for sound system; TODO remove
+		Orientation = glm::rotate(Orientation, glm::radians(180 * -rotX), glm::normalize(glm::cross(Orientation, Up)));
+		Orientation = glm::rotate(Orientation, glm::radians(180 * -rotY), Up);
+
+		yaw += rotX;
+		pitch += rotY;
+	}
 	//}
 
 	if (updated)
@@ -158,6 +164,34 @@ void Camera::Inputs(GLFWwindow* window)
 	speed = 10.0f;
 
 	//LOG_INFO("camera position: {0}, {1}, {2}", Position.x, Position.y, Position.z);
+
+	if (isOnRail) {
+		if (railTimer >= railMaxTime) {
+			railTimer = railMaxTime;
+			isOnRail = false;
+			isRailAngles = false;
+		}
+
+		float t = railTimer / railMaxTime * (railPoints.size() - 1);
+		Position = railSpline(t);
+
+		if (isRailAngles) {
+			t = railTimer / railMaxTime * (railAngles.size() - 1);
+			if (railAngles.size() <= 1 || t > railAngles.size() - 1) {
+				orientationQuat = railAngles[railAngles.size() - 1];
+				pitch = railPitch[railAngles.size() - 1];
+				yaw = railYaw[railAngles.size() - 1];
+			}
+			else {
+				orientationQuat = glm::mix(railAngles[int(t)], railAngles[int(t) + 1], t - int(t));
+				pitch = glm::mix(railPitch[int(t)], railPitch[int(t) + 1], t - int(t));
+				yaw = glm::mix(railYaw[int(t)], railYaw[int(t) + 1], t - int(t));
+			}
+		}
+
+		return;
+	}
+
 
 	if (autoScroll) {
 		if (autoScrollTimer >= autoScrollMaxTime) {
