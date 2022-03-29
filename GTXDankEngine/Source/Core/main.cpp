3726e66910a88a8f6389e451ca0348b30d38da61
cjmms <brandon.xyw@gmail.com>
2022-03-29 15:26:29 -0700

    hide the console (#274)
    * hide the console
    
    hide the console only in release mode
    
    * Fix debug mode

--- GTXDankEngine/Source/Core/main.cpp
+++ GTXDankEngine/Source/Core/main.cpp
@@ -2,10 +2,18 @@
 #include "Engine.h"
 #include "../utils/Log.h"
 
+
+
 Engine engine;
 
+#ifdef _DEBUG 
 int main()
+#else
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, char*, int nShowCmd)
+#endif
 {
+
+
 	Log::Init();	// this should be inside profiling system
 
 	engine.Init();
