3d34ff3bca38d74363d388bd7f3d73b5e5d621a3
Nathan Weise <nathan@weises.org>
2022-04-07 18:13:47 -0700

    sphere star

--- Platformer/Assets/Scripts/CategoryList.lua
+++ Platformer/Assets/Scripts/CategoryList.lua
@@ -1,5 +1,6 @@
 --categorylist
 function GetCategoryNames()
 	return {[1] = "Player", [2] = "Plate", [3] = "HoneySlime",[4] = "Launcher",
-	[5] = "Goal", [6] = "Translate", [7] = "Ball", [8] = "Glide", [9] = "Wind", [10] = "HomeSender" }
+	[5] = "Goal", [6] = "Translate", [7] = "Ball", [8] = "Glide", [9] = "Wind",
+	[10] = "HomeSender", [11] = "Star" }
 end
---
+++ Platformer/Assets/Scripts/DoStarThings.lua
@@ -0,0 +1,62 @@
+--Starthings
+local imguiControledEntity = -1
+local starStatus = {} -- 0.0 = new, 1.0 = collected
+
+
+function SaveStars( levelnum )
+	levelstr = string.format("%i", levelnum)
+	SaveIntFloatTableAsJson(starStatus, "/Assets/Levels/Level" .. levelstr .."/starStatusSave.json")
+	
+end
+
+function LoadStars( levelnum )
+	levelstr = string.format("%i", levelnum)
+	starStatus = LoadIntFloatTableFromJson("/Assets/Levels/Level" .. levelstr .."/starStatusSave.json")
+end
+
+function ClearStars()
+	starStatus = {}
+end
+
+function DestroyStar(e)
+	starStatus[e] = nil
+end
+
+function UpdateStar(dt, e)
+	if starStatus[e] == nil then
+		starStatus[e] = 0.0
+	end
+end
+
+function HandleEventStar(eventData)
+
+	if eventData.type == 16 then
+		ImguiText("Star")
+		imguiControledEntity = eventData.e1
+		ImguiControledFloat(0, "starStatus", starStatus[eventData.e1])
+	end
+	if eventData.type == 17 then
+		if imguiControledEntity ~= -1 then
+			starStatus[imguiControledEntity] = GetImguiControledFloat(0)
+			imguiControledEntity = -1
+		end
+		sum = 0
+		for index,value in pairs(starStatus) do
+			if value == 1.0 then
+				sum = sum +1
+			end
+		end
+		DrawText(sum.." Stars Collected", .7, 710,60, 1,1,0)
+	end
+	if eventData.type == 12 then
+		starStatus[eventData.e1] = 1.0
+		SetColliderShape(eventData.e1, 3)
+		SetWireFrame(eventData.e1, true)
+	end
+end
+
+
+
+function HandleEventPerEntityStar(e, eventData)
+
+end
